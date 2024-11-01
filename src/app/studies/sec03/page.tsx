072099ea18c4221df6920b041e0c468fad4c6526
HondaYt <honda.yuto.04@gmail.com>
2024-11-01 12:23:19 +0900

    add:section03

---
+++ src/app/studies/sec03/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Sec03() {
+	const [result, setResult] = useState(0);
+	const increment = () => {
+		setResult((prev) => prev + 1);
+	};
+	return (
+		<>
+			<p>a</p>
+		</>
+	);
+}
